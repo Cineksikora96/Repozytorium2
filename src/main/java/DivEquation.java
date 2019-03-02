970ee5473982563a01a2c54d54c3e67225297616
Cineksikora96 <48150243+Cineksikora96@users.noreply.github.com>
2019-03-02 14:58:22 +0100

    first commit

---
+++ src/main/java/DivEquation.java
@@ -0,0 +1,7 @@
+public class DivEquation implements ICalculable {
+    public double calculate() {
+        return a / b;
+    }
+    private double a;
+    private double b;
+}
---
+++ src/main/java/MulEquation.java
@@ -0,0 +1,7 @@
+public class MulEquation implements ICalculable {
+    public double calculate() {
+        return a * b;
+    }
+    private double a;
+    private double b;
+}
