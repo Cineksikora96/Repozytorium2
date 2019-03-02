dcccfe98a2c2ad56ce966055fc3ff89d41b986ba
Filip Karcz <filip.karcz@gmail.com>
2019-03-02 15:01:57 +0100

    dodanie konstruktorow w main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+
 public class Main {
     public static void main(String[] args) {
 
+        SumEquation add = new SumEquation();
+        SubstractEquation sub = new SubstractEquation();
+        DivEquation div = new DivEquation();
+        MulEquation mul = new MulEquation();
     }
 }
