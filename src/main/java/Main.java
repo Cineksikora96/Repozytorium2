b9b19611383d39d71c199161da2f04dfa97203f7
Filip Karcz <filip.karcz@gmail.com>
2019-03-02 15:27:53 +0100

    dodanie dzialan w main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        SumEquation add = new SumEquation(10, 2);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Podaj dzialanie (add, substract, div, mul)");
+        String dzialanie = sc.nextLine();
+        System.out.println("Podaj liczbe 1:");
+        double liczba1 = Integer.parseInt(sc.nextLine());
+        System.out.println("Podaj liczbe 2:");
+        double liczba2 = Integer.parseInt(sc.nextLine());
+
+        while (!dzialanie.equals("quit")) {
+        if (dzialanie.equals("add")) {
+            SumEquation add = new SumEquation(liczba1, liczba2);
+            System.out.println(add.calculate());
+        } else if (dzialanie.equals("substract")) {
+            SubstractEquation sub = new SubstractEquation(liczba1, liczba2);
+            System.out.println(sub.calculate());
+        } else if (dzialanie.equals("divide")) {
+            DivEquation div = new DivEquation(liczba1, liczba2);
+            System.out.println(div.calculate());
+        } else if (dzialanie.equals("mul")) {
+            MulEquation mul = new MulEquation(liczba1, liczba2);
+            System.out.println(mul.calculate());
+        } else { PowEquation pow = new PowEquation(liczba1, liczba2);
+            System.out.println(pow.calculate());
+        }
+        }
     }
 }
